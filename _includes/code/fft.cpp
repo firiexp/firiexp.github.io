b2b45df0a9dc09bcabb9f355db8a4cdf21e05773
firiexp <firizfiriz300@hotmail.co.jp>
2019-04-28 21:44:35 +0900

    fix

--- _includes/code/fft.cpp
+++ _includes/code/fft.cpp
@@ -1,19 +1,3 @@
-template<class T>
-vector<T> divisor(T n){
-    vector<T> ret;
-    for(T i = 1; i * i <= n; i++) {
-        if(n % i == 0) {
-            ret.push_back(i);
-            if(i * i != n) ret.push_back(n / i);
-        }
-    }
-    sort(begin(ret), end(ret));
-    return(ret);
-}
-FFT(高速フーリエ変換)
-ほぼ丸写し　arrayに置き換えるとコンパイル時間が爆発します
-Polyがちょっと扱いづらいかも。
-
 #include <cmath>
 namespace FFT {
     const int max_base = 19, maxN = 1 << max_base; // N <= 2e5
