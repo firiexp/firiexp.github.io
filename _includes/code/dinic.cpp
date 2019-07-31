f12ebe7bb342cd83436e7891066d357f077100dd
firiexp <firizfiriz300@hotmail.co.jp>
2019-07-31 21:56:11 +0900

    fix dinic

--- _includes/code/dinic.cpp
+++ _includes/code/dinic.cpp
@@ -15,11 +15,11 @@ class Dinic {
             }
         }
     }
-
+ 
     T dfs(int v, int t, T f){
         if(v == t) return f;
         for(int &i = iter[v]; i < G[v].size(); i++){
-            edge e = G[v][i];
+            edge &e = G[v][i];
             if(e.cap > 0 && level[v] < level[e.to]){
                 T d = dfs(e.to, t, min(f,  e.cap));
                 if(d == 0) continue;
@@ -36,18 +36,18 @@ public:
         edge() = default;
         edge(int to, T cap, int rev) : to(to), cap(cap), rev(rev) {}
     };
-
+ 
     vector<vector<edge>> G;
     vector<int> level, iter;
     Dinic() = default;
     explicit Dinic(int n) : G(n), level(n), iter(n) {}
-
-    bool add_edge(int from, int to, int cap){
+ 
+    void add_edge(int from, int to, int cap){
         G[from].emplace_back(to, cap, G[to].size());
-        G[to].emplace_bacl(from, directed ? 0 : cap,  G[from].size()-1);
+        G[to].emplace_back(from, directed ? 0 : cap,  G[from].size()-1);
     }
-
-    
+ 
+ 
     T flow(int s, int t, T lim = INF<T>){
         T ret = 0;
         while(true) {
