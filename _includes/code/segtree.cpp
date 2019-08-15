639f53a9fe2ed0d55ea06a4fc23f987fd7cadda6
firiexp <firiexp@PC.localdomain>
2019-08-15 21:39:08 +0900

    add find to segtree

--- _includes/code/segtree.cpp
+++ _includes/code/segtree.cpp
@@ -8,19 +8,19 @@ struct SegmentTree{
         while(sz < n) sz <<= 1;
         seg.assign(2*sz, M::e());
     }
- 
+
     void set(int k, const T &x){ seg[k + sz] = x; }
- 
+
     void build(){
         for (int i = sz-1; i > 0; --i) seg[i] = M::f(seg[2*i], seg[2*i+1]);
     }
- 
+
     void update(int k, const T &x){
         k += sz;
         seg[k] = x;
         while (k >>= 1) seg[k] = M::f(seg[2*k], seg[2*k+1]);
     }
- 
+
     T query(int a, int b){
         T l = M::e(), r = M::e();
         for(a += sz, b += sz; a < b; a >>=1, b>>=1){
@@ -29,11 +29,30 @@ struct SegmentTree{
         }
         return M::f(l, r);
     }
- 
+
+    template<typename C>
+    int find(int t, C &c, T &val, int k, int l, int r){
+        if(r-l == 1){
+            val = f(val, seg[k]);
+            return c(val) ? k-sz : -1;
+        }
+        int m = (l+r) >> 1;
+        if(m <= t) return find(t, c, val, (k << 1) | 1, m, r);
+        if(t <= l && !c(val = f(val, seg[k]))) return -1;
+        int lv = find(t, c, val, (k << 1) | 0 , l, m);
+        if(~lv) return lv;
+        return find(t, c, val, (k << 1) | 1, m, r);
+    }
+
+    template<typename C>
+    int find(int t, C &c){
+        T val = M::e();
+        return find(t, c, val, 1, 0, sz);
+    }
     T operator[](const int &k) const { return seg[k + sz]; }
 };
- 
- 
+
+
 struct Monoid{
     using T = int;
     static T f(T a, T b) { return min(a, b); }
