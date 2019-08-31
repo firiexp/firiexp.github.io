655b5181b32e60daf0c152586dd507f2cea89ead
firiexp <firiexp@PC.localdomain>
2019-08-31 19:25:49 +0900

    add modint to factorial

--- _includes/code/factorial.cpp
+++ _includes/code/factorial.cpp
@@ -1,48 +1,76 @@
-template <class T>
-T pow_ (T x, T n, T M){
-    uint64_t u = 1, xx = x;
-    while (n > 0){
-        if (n&1) u = u * xx % M;
-        xx = xx * xx % M;
-        n >>= 1;
+template <ll M = MOD> struct modint {
+    ll val;
+    modint(const ll x = 0) : val(x) {
+        val = x;
+        while(val < 0) val += M;
+        while(val > M) val -= M;
     }
-    return static_cast<T>(u);
+
+    modint operator+(const modint a) const { return modint(*this) += a; }
+    modint operator-(const modint a) const { return modint(*this) -= a; }
+    modint operator*(const modint a) const { return modint(*this) *= a; }
+    modint operator/(const modint a) const { return modint(*this) /= a; }
+    modint operator-() const { return modint(M-val); }
+    modint inv() const {
+        ll u = 1, v = 0, s = 0, t = 1, m = M, x = val;
+        while (x) {ll q = m/x; swap(s -= q*u, u); swap(t -= q*v, v); swap(m -= q*x, x); }
+        if(s < 0) s += M;
+        return modint(s);
+    }
+    modint pow(ll n) const {
+        ll u = 1, xx = val;
+        while (n > 0){ if (n&1) u = u * xx % M; xx = xx * xx % M; n >>= 1; }
+        return modint(u);
+    }
+    modint& operator+=(const modint a){ val += a.val; if(val >= M) val -= M; return *this; }
+    modint& operator-=(const modint a){ val -= a.val; if(val < 0) val += M; return *this; }
+    modint& operator*=(const modint a){ val = val * a.val % M; return *this; }
+    modint& operator/=(const modint a){ val = val * a.inv().val % M; return *this;}
+
+    modint& operator=(const int& x){
+        val = x;
+        while(val < 0) val += M;
+        while(val > M) val -= M;
+        return *this;
+    }
 };
 
-template <class T> class Factorial {
-    T mod;
-    vector<uint64_t> facts, factinv;
+
+class Factorial {
+    using mint = modint<MOD>;
+    vector<mint> facts, factinv;
 
 public:
-    Factorial(int n, T mod) : facts(static_cast<u32>(n+1)), factinv(static_cast<u32>(n+1)), mod(mod) {
+    explicit Factorial(int n) : facts(static_cast<u32>(n+1)), factinv(static_cast<u32>(n+1)) {
         facts[0] = 1;
-        for (int i = 1; i < n+1; ++i) facts[i] = facts[i-1]*i % mod;
-        factinv[n] = pow_(facts[n], static_cast<uint64_t>(mod - 2), static_cast<uint64_t>(mod));
-        for (int i = n-1; i >= 0; --i) factinv[i] = factinv[i+1] * (i+1) % mod;
+        for (int i = 1; i < n+1; ++i) facts[i] = facts[i-1]*mint(i);
+        factinv[n] = facts[n].inv();
+        for (int i = n-1; i >= 0; --i) factinv[i] = factinv[i+1] * mint(i+1);
     }
 
-    T fact(int k) const {
-        if(k >= 0) return static_cast<T>(facts[k]);
-        else return static_cast<T>(factinv[-k]);
+    mint fact(int k) const {
+        if(k >= 0) return facts[k]; else return factinv[-k];
     }
 
-    T operator[](const int &k) const {
-        if(k >= 0) return static_cast<T>(facts[k]);
-        else return static_cast<T>(factinv[-k]);
+    mint operator[](const int &k) const {
+        if(k >= 0) return facts[k]; else return factinv[-k];
     }
 
-    T C(int p, int q) const {
+    mint C(int p, int q) const {
         if(q < 0 || p < q) return 0;
-        return static_cast<T>(facts[p]*  factinv[q] % mod * factinv[p-q] % mod);
+        return facts[p] * factinv[q] * factinv[p-q];
     }
 
-    T P(int p, int q) const {
+    mint P(int p, int q) const {
         if(q < 0 || p < q) return 0;
-        return static_cast<T>((facts[p] * factinv[p-q]) % mod);
+        return facts[p] * factinv[p-q];
     }
 
-    T H(int p, int q) const {
+    mint H(int p, int q) const {
         if(p < 0 || q < 0) return 0;
-        return static_cast<T>(q == 0 ? 1 : C(p+q-1, q));
+        return q == 0 ? 1 : C(p+q-1, q);
     }
 };
+
+
+using mint = modint<MOD>;
