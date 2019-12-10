415f51097e9dd86faf41b21aeb46887b7a6ff46d
firiexp <firizfiriz300@hotmail.co.jp>
2019-12-10 20:28:35 +0900

    factorial modint u32

--- _includes/code/factorial.cpp
+++ _includes/code/factorial.cpp
@@ -1,76 +1,62 @@
-template <ll M = MOD> struct modint {
-    ll val;
-    modint(const ll x = 0) : val(x) {
-        val = x;
-        while(val < 0) val += M;
-        while(val > M) val -= M;
-    }
+template<u32 M = 1000000007> 
+struct modint{
+    u32 val;
+    modint(): val(0){}
+    template<typename T>
+    modint(T t){t %= (T)M; if(t < 0) t += (T)M; val = t;}
 
-    modint operator+(const modint a) const { return modint(*this) += a; }
-    modint operator-(const modint a) const { return modint(*this) -= a; }
-    modint operator*(const modint a) const { return modint(*this) *= a; }
-    modint operator/(const modint a) const { return modint(*this) /= a; }
-    modint operator-() const { return modint(M-val); }
-    modint inv() const {
-        ll u = 1, v = 0, s = 0, t = 1, m = M, x = val;
-        while (x) {ll q = m/x; swap(s -= q*u, u); swap(t -= q*v, v); swap(m -= q*x, x); }
-        if(s < 0) s += M;
-        return modint(s);
+    modint pow(ll k) const {
+        modint res(1), x(val);
+        while(k){
+            if(k&1) res *= x;
+            x *= x;
+            k >>= 1;
+        }
+        return res;
     }
-    modint pow(ll n) const {
-        ll u = 1, xx = val;
-        while (n > 0){ if (n&1) u = u * xx % M; xx = xx * xx % M; n >>= 1; }
-        return modint(u);
-    }
-    modint& operator+=(const modint a){ val += a.val; if(val >= M) val -= M; return *this; }
-    modint& operator-=(const modint a){ val -= a.val; if(val < 0) val += M; return *this; }
-    modint& operator*=(const modint a){ val = val * a.val % M; return *this; }
-    modint& operator/=(const modint a){ val = val * a.inv().val % M; return *this;}
-
-    modint& operator=(const int& x){
-        val = x;
-        while(val < 0) val += M;
-        while(val > M) val -= M;
-        return *this;
-    }
+    template<typename T>
+    modint& operator=(T t){t %= (T)M; if(t < 0) t += (T)M; val = t; return *this; }
+    modint inv() const {return pow(M-2);}
+    modint& operator+=(modint a){ val += a.val; if(val >= M) val -= M; return *this;}
+    modint& operator-=(modint a){ if(val < a.val) val += M-a.val; else val -= a.val; return *this;}
+    modint& operator*=(modint a){ val = (u64)val*a.val%M; return *this;}
+    modint& operator/=(modint a){ return (*this) *= a.inv();}
+    modint operator+(modint a) const {return modint(val) +=a;}
+    modint operator-(modint a) const {return modint(val) -=a;}
+    modint operator*(modint a) const {return modint(val) *=a;}
+    modint operator/(modint a) const {return modint(val) /=a;}
+    modint operator-(){ return modint(M-val);}
+    bool operator==(const modint a) const {return val == a.val;}
+    bool operator!=(const modint a) const {return val != a.val;}
+    bool operator<(const modint a) const {return val < a.val;}
 };
-
+using mint = modint<MOD>;
 
 class Factorial {
-    using mint = modint<MOD>;
     vector<mint> facts, factinv;
-
 public:
-    explicit Factorial(int n) : facts(static_cast<u32>(n+1)), factinv(static_cast<u32>(n+1)) {
+    explicit Factorial(int n) : facts(n+1), factinv(n+1) {
         facts[0] = 1;
-        for (int i = 1; i < n+1; ++i) facts[i] = facts[i-1]*mint(i);
+        for (int i = 1; i < n+1; ++i) facts[i] = facts[i-1] * mint(i);
         factinv[n] = facts[n].inv();
         for (int i = n-1; i >= 0; --i) factinv[i] = factinv[i+1] * mint(i+1);
     }
-
     mint fact(int k) const {
         if(k >= 0) return facts[k]; else return factinv[-k];
     }
-
     mint operator[](const int &k) const {
         if(k >= 0) return facts[k]; else return factinv[-k];
     }
-
     mint C(int p, int q) const {
         if(q < 0 || p < q) return 0;
         return facts[p] * factinv[q] * factinv[p-q];
     }
-
     mint P(int p, int q) const {
         if(q < 0 || p < q) return 0;
         return facts[p] * factinv[p-q];
     }
-
     mint H(int p, int q) const {
         if(p < 0 || q < 0) return 0;
         return q == 0 ? 1 : C(p+q-1, q);
     }
 };
-
-
-using mint = modint<MOD>;
