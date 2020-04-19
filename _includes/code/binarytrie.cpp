2a589debcc332bd0c2a109ddcd7f83c887f588a3
firiexp <firizfiriz300@hotmail.co.jp>
2020-04-19 16:38:54 +0900

    add binary trie

--- _includes/code/binarytrie.cpp
+++ _includes/code/binarytrie.cpp
@@ -1,10 +1,10 @@
 struct Node {
     int val;
-    Node *l, *r;
-    Node() : val(0), l(nullptr), r(nullptr) {}
+    Node *nxt[2];
+    Node() : val(0), nxt{nullptr, nullptr} {}
     static int cnt;
     void *operator new(size_t){
-        static Node pool[3200000];
+        static Node pool[3000000];
         return pool + cnt++;
     }
 };
@@ -15,14 +15,25 @@ struct Binarytrie {
     Node *root;
     Binarytrie() { root = new Node; }
 
-    void add(const T b, int x = 1){
+    void add(const T b, T x = 1){
         Node *p = root;
         for (int i = X-1; i >= 0; --i) {
             bool f = (b >> i) & 1;
-            if(!f && !p->l) p->l = new Node;
-            if(f && !p->r) p->r = new Node;
-            p = (f ? p->r : p->l);
+            if(!p->nxt[f]) p->nxt[f] = new Node;
+            p = p->nxt[f];
         }
         p->val += x;
+    }
+
+    T xor_min(Node* t, const T &x, int dep = X-1){
+        if(dep < 0) return 0;
+        T f = (x >> dep) & 1U; f ^= !t->nxt[f];
+        return xor_min(t->nxt[f], x, dep-1) | (f << dep);
+    }
+    T max_element(T x = 0) {
+        return xor_min(root, ~x);
+    }
+    T min_element(T x = 0){
+        return xor_min(root, x);
     }
 };
